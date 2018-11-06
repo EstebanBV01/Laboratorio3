90a54b999b895637ccb7ac0eade6456921836bd2
usuario <usuario@AFila4PC1>
2018-11-06 09:24:03 -0600

    Cambios de static

--- Lab3_B81023_EstebanBarrantes_B86597_JerryRivera/src/lab3_b81023_estebanbarrantes_b86597_jerryrivera/NationalMonument.java
+++ Lab3_B81023_EstebanBarrantes_B86597_JerryRivera/src/lab3_b81023_estebanbarrantes_b86597_jerryrivera/NationalMonument.java
@@ -9,6 +9,41 @@ package lab3_b81023_estebanbarrantes_b86597_jerryrivera;
  *
  * @author Usuario
  */
-public class NationalMonument {
+public abstract class NationalMonument extends NationalPark implements Income {
+    private double publicIncomes;
+    private double privateIncomes;
+    
+   
+    public NationalMonument (String name,String location, double total, double publicIncomes, double privateIncomes, int electricityUsers, int nationalVisitors, int foreignVisitors){
+        super(name, location, total, electricityUsers, nationalVisitors, foreignVisitors);
+        this.privateIncomes = privateIncomes;
+        this.publicIncomes = publicIncomes;
+    }
+
+    public double getPublicIncomes() {
+        return publicIncomes;
+    }
+
+    public void setPublicIncomes(double publicIncomes) {
+        this.publicIncomes = publicIncomes;
+    }
+
+    public double getPrivateIncomes() {
+        return privateIncomes;
+    }
+
+    public void setPrivateIncomes(double privateIncomes) {
+        this.privateIncomes = privateIncomes;
+    }
+    
+    @Override
+    public double totalIncomes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void privateIncome() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
